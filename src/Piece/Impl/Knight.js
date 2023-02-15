ea60c569b80dae91f35363a0710bd3df0cf732bd
Xwilarg <xwilarg@protonmail.com>
2023-02-15 21:27:42 +0100

    Implement jump

--- src/Piece/Impl/Knight.js
+++ src/Piece/Impl/Knight.js
@@ -1,9 +1,9 @@
 import { Move } from "../Move";
-import { Move_Walk } from "../MoveType";
+import { Move_Jump } from "../MoveType";
 import { Piece } from "../Piece";
 import { Vector2 } from "../Vector2";
 
 export const BearsEyes = new Piece("桂馬", "Knight", "ffN", [
-    new Move(Move_Walk, new Vector2(2, 1), 1),
-    new Move(Move_Walk, new Vector2(2, 1), -1)
+    new Move(Move_Jump, new Vector2(2, 1), 1),
+    new Move(Move_Jump, new Vector2(2, 1), -1)
 ]);
