617def641bcc94aebc300941e4d77fa203717516
Xwilarg <xwilarg@protonmail.com>
2023-02-16 14:30:55 +0100

    Fix knight move

--- src/Piece/Impl/Knight.js
+++ src/Piece/Impl/Knight.js
@@ -3,4 +3,4 @@ import { moveJump } from "../MoveType";
 import { Piece } from "../Piece";
 import { Vector2 } from "../Vector2";
 
-export const Knight = new Piece("桂馬", "Knight", "ffN", [new Move(moveJump, new Vector2(2, 1), 1), new Move(moveJump, new Vector2(2, 1), -1)]);
+export const Knight = new Piece("桂馬", "Knight", "ffN", [new Move(moveJump, new Vector2(1, 2), 1), new Move(moveJump, new Vector2(-1, 2), 1)]);
