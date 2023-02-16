13f31ddb360dab6f4af2731c3ae09e51b9e27965
Xwilarg <xwilarg@protonmail.com>
2023-02-16 11:57:45 +0100

    Fix piece name

--- src/Piece/Impl/Knight.js
+++ src/Piece/Impl/Knight.js
@@ -3,7 +3,7 @@ import { Move_Jump } from "../MoveType";
 import { Piece } from "../Piece";
 import { Vector2 } from "../Vector2";
 
-export const BearsEyes = new Piece("桂馬", "Knight", "ffN", [
+export const Knight = new Piece("桂馬", "Knight", "ffN", [
     new Move(Move_Jump, new Vector2(2, 1), 1),
     new Move(Move_Jump, new Vector2(2, 1), -1)
 ]);
